5a6d0abde7dfdd2bb2643ecf3609bc2e446da4e8
James Willock <james.willock@gmail.com>
2019-02-28 12:52:01 +0000

    Search through a large collection

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const { shuffle, selectionSort, findSmallest } = require('./sorting');
 const { binarySearch, stupidSearch } = require('./search');
 
 // Create collection of 10000 integers
-const numbers = filledArray(1000),
-      finalNumber = numbers[numbers.length - 1];
+const sortNumbers = filledArray(1000),
+      finalSortNumber = sortNumbers[sortNumbers.length - 1];
 
 // Shuffled using `shuffle()`
 console.time("shuffle()");
-const shuffled = shuffle(numbers);
+const shuffled = shuffle(sortNumbers);
 console.timeEnd("shuffle()");
 
 // Find smallest index
@@ -22,14 +22,18 @@ console.time("selectionSort()");
 const sorted = selectionSort(shuffled);
 console.timeEnd("selectionSort()");
 
+// Instantiate a million integers in an array
+const searchNumbers = filledArray(1000000),
+      finalSearchNumber = searchNumbers[searchNumbers.length - 1];
+
 // Stupid search for member
 console.time("stupidSearch()");
-const stupidSearchIndex = stupidSearch(numbers, finalNumber);
+const stupidSearchIndex = stupidSearch(searchNumbers, finalSearchNumber);
 console.timeEnd("stupidSearch()");
 console.log("stupidSearch returned", stupidSearchIndex);
 
 // Binary search for member
 console.time("binarySearch()");
-const binarySearchIndex = stupidSearch(numbers, finalNumber);
+const binarySearchIndex = binarySearch(searchNumbers, finalSearchNumber);
 console.timeEnd("binarySearch()");
 console.log("binarySearch returned", binarySearchIndex);
