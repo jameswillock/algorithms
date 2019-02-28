f09d4182f2a91ea94b16bdb55fc0ebb643f7cf33
James Willock <james.willock@gmail.com>
2019-02-28 15:06:52 +0000

    Add some tests for searching functions

---
+++ test/search.test.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+const { binarySearch, stupidSearch } = require('../search');
+
+describe('stupidSearch', () => {
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
+  context('when member exists in collection', () => {
+    const find = numbers[numbers.length - 1];
+    
+    it('should return the index of the item', function() {
+      const foundIndex = stupidSearch(numbers, find);
+      assert.equal(foundIndex, numbers.length - 1);
+    });
+  });
+
+  context('when member is missing from collection', () => {
+    const find = 11;
+    
+    it('should return null', function() {
+      const foundIndex = stupidSearch(numbers, find);
+      assert.equal(foundIndex, null);
+    });
+  });
+});
+
+describe('binarySearch', () => {
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
+  context('when member exists in collection', () => {
+    const find = numbers[numbers.length - 1];
+  
+    it('should return the index of the item', function() {
+      const foundIndex = binarySearch(numbers, find);
+      assert.equal(foundIndex, numbers.length - 1);
+    });
+  });
+
+  context('when member is missing from collection', () => {
+    const find = 11;
+    
+    it('should return null', function() {
+      const foundIndex = binarySearch(numbers, find);
+      assert.equal(foundIndex, null);
+    });
+  });
+});
