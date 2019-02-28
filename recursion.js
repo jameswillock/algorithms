215b85a6bb92b2864965103820a0c1fab96cb0f8
James Willock <james.willock@gmail.com>
2019-02-28 15:28:25 +0000

    Fixed formatting of recursion.js

--- recursion.js
+++ recursion.js
@@ -1,50 +1,50 @@
 // Count the number of members in a collection
 const recursiveLength = collection => {
-	// If the collection is empty, return zero
-	if (!collection.length) return 0;
-	
-	// If the collection is non-empty, add one
-	// to the length of the rest of the collection
-	// obtained via recursion
-	return 1 + recursiveLength(collection.slice(1));
+  // If the collection is empty, return zero
+  if (!collection.length) return 0;
+  
+  // If the collection is non-empty, add one
+  // to the length of the rest of the collection
+  // obtained via recursion
+  return 1 + recursiveLength(collection.slice(1));
 };
 
 // Count the number of members in a collection without recursion
 const nonRecursiveLength = collection => {
-	let count = 0;
-	
-	for (let index = 0; index < collection.length; index++) {
-		count++;
-	}
-	
-	return count;
+  let count = 0;
+  
+  for (let index = 0; index < collection.length; index++) {
+    count++;
+  }
+  
+  return count;
 };
 
 // Sum the contents of an array using recursion
 const recursiveSumArray = collection => {
-	// If the collection is empty, return zero
-	if (!collection.length) return 0;
-	
-	// If the collection is non-empty, shift the first
-	// element, and add it to the sum'd value of the rest
-	// of the collection obtained via recursion
-	return collection.shift() + recursiveSumArray(collection);
+  // If the collection is empty, return zero
+  if (!collection.length) return 0;
+  
+  // If the collection is non-empty, shift the first
+  // element, and add it to the sum'd value of the rest
+  // of the collection obtained via recursion
+  return collection.shift() + recursiveSumArray(collection);
 };
 
 // Sum the contents of an array without recursion
 const nonRecursiveSumArray = collection => {
-	let sum = collection[0];
+  let sum = collection[0];
 
-	for (let index = 1; index < collection.length; index++) {
-		sum += collection[index];
-	}
-	
-	return sum;
+  for (let index = 1; index < collection.length; index++) {
+    sum += collection[index];
+  }
+  
+  return sum;
 };
 
 module.exports = {
-	nonRecursiveSumArray,
-	recursiveSumArray,
-	nonRecursiveLength,
-	recursiveLength
+  nonRecursiveSumArray,
+  recursiveSumArray,
+  nonRecursiveLength,
+  recursiveLength
 };
