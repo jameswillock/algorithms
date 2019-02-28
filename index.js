61beb925963a3d08ee1fc8def140c22bf301783a
James Willock <james.willock@gmail.com>
2019-02-28 14:47:07 +0000

    Added some more recursive functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { filledArray } = require('./utilities');
 const { shuffle, selectionSort, findSmallest } = require('./sorting');
 const { binarySearch, stupidSearch } = require('./search');
+const { nonRecursiveSumArray, recursiveSumArray, recursiveLength, nonRecursiveLength } = require('./recursion'); 
 
 // Create collection of 10000 integers
 const sortNumbers = filledArray(1000),
@@ -22,8 +23,8 @@ console.time("selectionSort()");
 const sorted = selectionSort(shuffled);
 console.timeEnd("selectionSort()");
 
-// Instantiate a million integers in an array
-const searchNumbers = filledArray(1000000),
+// Instantiate 100_000 integers in an array
+const searchNumbers = filledArray(100000),
       finalSearchNumber = searchNumbers[searchNumbers.length - 1];
 
 // Stupid search for member
@@ -37,3 +38,30 @@ console.time("binarySearch()");
 const binarySearchIndex = binarySearch(searchNumbers, finalSearchNumber);
 console.timeEnd("binarySearch()");
 console.log("binarySearch returned", binarySearchIndex);
+
+const recursiveSumNumbers = filledArray(100);
+const nonRecursiveSumNumbers = recursiveSumNumbers.slice(0);
+
+// Sum array via recursion
+console.time("recursiveSumArray()");
+const recursiveSum = recursiveSumArray(recursiveSumNumbers);
+console.timeEnd("recursiveSumArray()");
+console.log("recursiveSumArray returned", recursiveSum);
+
+// Sum array without recursion
+console.time("nonRecursiveSumArray()");
+const nonRecursiveSum = nonRecursiveSumArray(nonRecursiveSumNumbers);
+console.timeEnd("nonRecursiveSumArray()");
+console.log("nonRecursiveSumArray returned", nonRecursiveSum);
+
+// Count the length of an array via recursion
+const countArray = filledArray(100);
+console.time("recursiveLength()");
+const countArrayLength = recursiveLength(countArray);
+console.timeEnd("recursiveLength()");
+console.log("recursiveLength returned", countArrayLength);
+
+console.time("nonRecursiveLength()");
+const countArrayLength2 = nonRecursiveLength(countArray);
+console.timeEnd("nonRecursiveLength()");
+console.log("nonRecursiveLength returned", countArrayLength2);
--- recursion.js
+++ recursion.js
@@ -1,14 +1,50 @@
-// Recursion
-
-// Simple countdown – print number to console until we reach zero
-const countdown = (number) => {
-	console.log(number);
-
-	// Return if zero, to prevent infinite loop
-	if (number <= 0) return;
+// Count the number of members in a collection
+const recursiveLength = collection => {
+	// If the collection is empty, return zero
+	if (!collection.length) return 0;
 	
-	// Recursion!
-	countdown(number - 1);
+	// If the collection is non-empty, add one
+	// to the length of the rest of the collection
+	// obtained via recursion
+	return 1 + recursiveLength(collection.slice(1));
 };
 
-module.exports = { countdown };
+// Count the number of members in a collection without recursion
+const nonRecursiveLength = collection => {
+	let count = 0;
+	
+	for (let index = 0; index < collection.length; index++) {
+		count++;
+	}
+	
+	return count;
+};
+
+// Sum the contents of an array using recursion
+const recursiveSumArray = collection => {
+	// If the collection is empty, return zero
+	if (!collection.length) return 0;
+	
+	// If the collection is non-empty, shift the first
+	// element, and add it to the sum'd value of the rest
+	// of the collection obtained via recursion
+	return collection.shift() + recursiveSumArray(collection);
+};
+
+// Sum the contents of an array without recursion
+const nonRecursiveSumArray = collection => {
+	let sum = collection[0];
+
+	for (let index = 1; index < collection.length; index++) {
+		sum += collection[index];
+	}
+	
+	return sum;
+};
+
+module.exports = {
+	nonRecursiveSumArray,
+	recursiveSumArray,
+	nonRecursiveLength,
+	recursiveLength
+};
