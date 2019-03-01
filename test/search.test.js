8be9b53059d07ecabee8b65045618cf3d89a7c1c
James Willock <james.willock@gmail.com>
2019-03-01 15:40:57 +0000

    Refactor search tests

--- test/search.test.js
+++ test/search.test.js
@@ -1,45 +1,38 @@
 const assert = require('assert');
 const { binarySearch, stupidSearch } = require('../search');
 
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      find = numbers[numbers.length - 1],
+      missing = 11;
+
 describe('stupidSearch', () => {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  
   context('when member exists in collection', () => {
-    const find = numbers[numbers.length - 1];
-    
     it('should return the index of the item', () => {
       const foundIndex = stupidSearch(numbers, find);
-      assert.equal(foundIndex, numbers.length - 1);
+      assert.equal(foundIndex, find);
     });
   });
 
   context('when member is missing from collection', () => {
-    const find = 11;
     
     it('should return null', function() {
-      const foundIndex = stupidSearch(numbers, find);
+      const foundIndex = stupidSearch(numbers, missing);
       assert.equal(foundIndex, null);
     });
   });
 });
 
 describe('binarySearch', () => {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  
   context('when member exists in collection', () => {
-    const find = numbers[numbers.length - 1];
-  
     it('should return the index of the item', () => {
       const foundIndex = binarySearch(numbers, find);
-      assert.equal(foundIndex, numbers.length - 1);
+      assert.equal(foundIndex, find);
     });
   });
 
   context('when member is missing from collection', () => {
-    const find = 11;
-    
     it('should return null', function() {
-      const foundIndex = binarySearch(numbers, find);
+      const foundIndex = binarySearch(numbers, missing);
       assert.equal(foundIndex, null);
     });
   });
